dcab373d43d10bbb3f07ed350ae52f90c2ba9cfe
UndeadButterfly <knight2944731@gmail.com>
2023-02-01 17:53:36 +0900

    page assemble

---
+++ solar_embassy_community/src/main/java/com/community/solar_embassy/controller/GalaxyController.java
@@ -0,0 +1,12 @@
+package com.community.solar_embassy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/galaxy")
+public class GalaxyController {
+    @GetMapping("/autopedia.do")
+    public void autopedia(){}
+}
