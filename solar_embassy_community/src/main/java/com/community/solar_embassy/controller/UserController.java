3f2a2fc18ff4ce03931ed9276ae6f34281164b2b
UndeadButterfly <knight2944731@gmail.com>
2023-02-04 15:10:38 +0900

    db sql complete

--- solar_embassy_community/src/main/java/com/community/solar_embassy/controller/UserController.java
+++ solar_embassy_community/src/main/java/com/community/solar_embassy/controller/UserController.java
@@ -1,14 +1,31 @@
 package com.community.solar_embassy.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     @GetMapping("login.do")
-    public void login(){}
+    public void login() {
+    }
+
     @GetMapping("withdrawl.do")
-    public void withdrawal(){}
+    public void withdrawal() {
+    }
+
+    @GetMapping("signup.do")
+    public void signup() {
+    }
+
+    @GetMapping("signup_normal.do")
+    public void signup_normal() {
+    }
+    @GetMapping("signup_business.do")
+    public void signup_business() {
+    }
+
 }
