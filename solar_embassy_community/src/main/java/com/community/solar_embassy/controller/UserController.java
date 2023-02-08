027733515819286e9da7121b582d55f58ab06320
UndeadButterfly <knight2944731@gmail.com>
2023-02-08 14:41:57 +0900

    login

--- solar_embassy_community/src/main/java/com/community/solar_embassy/controller/UserController.java
+++ solar_embassy_community/src/main/java/com/community/solar_embassy/controller/UserController.java
@@ -1,19 +1,57 @@
 package com.community.solar_embassy.controller;
 
+import com.community.solar_embassy.dto.Users;
+import com.community.solar_embassy.service.UsersService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    @GetMapping("login.do")
-    public void login() {
+
+    private UsersService usersService;
+
+    public UserController(UsersService usersService) {
+        this.usersService = usersService;
     }
 
-    @GetMapping("withdrawl.do")
+    @GetMapping("login.do")
+    public void login(HttpServletRequest req,
+                      HttpSession session,
+                      @SessionAttribute(required = false) String redirectUri) {
+        String referer = req.getHeader("referer");
+        if (redirectUri == null && !(referer.equals("http://localhost:8888/") || referer.equals("http://localhost:8888/user/login.do"))) {
+            session.setAttribute("redirectUri",referer);
+        }
+    }
+    @PostMapping("/login.do")
+    public String login(
+            @RequestParam(name = "user_id") String userId,
+            String pw,
+            HttpSession session,
+            @SessionAttribute(required = false) String redirectUri){
+        Users user=usersService.login(userId,pw);
+        session.setAttribute("loginUser",user);
+        if(user==null){
+            session.setAttribute("msg","아이디나 비밀번호를 확인하세요!");
+            return "redirect:/user/login.do";
+        }else{
+            if(redirectUri==null){
+                return "redirect:/";
+            }
+            session.removeAttribute("redirectUri");
+            return "redirect:"+redirectUri;
+        }
+    }
+    @GetMapping("/logout.do")
+    public String logout(HttpSession session){
+        session.removeAttribute("loginUser");
+        return "redirect:/";
+    }
+
+    @GetMapping("withdrawal.do")
     public void withdrawal() {
     }
 
--- solar_embassy_community/src/main/java/com/community/solar_embassy/service/UsersService.java
+++ solar_embassy_community/src/main/java/com/community/solar_embassy/service/UsersService.java
@@ -1,5 +1,8 @@
 package com.community.solar_embassy.service;
 
+import com.community.solar_embassy.dto.Users;
+
 public interface UsersService {
 
+    Users login(String userId, String pw);
 }
