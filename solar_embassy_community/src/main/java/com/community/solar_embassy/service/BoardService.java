507d1409197511b56686d7f94da4fafc934001a7
내MoonHyunHee <judge030@naver.com>
2023-02-11 13:55:12 +0900

    게시판 작성 및 저장 성공

--- solar_embassy_community/src/main/java/com/community/solar_embassy/service/BoardService.java
+++ solar_embassy_community/src/main/java/com/community/solar_embassy/service/BoardService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface BoardService  {
 
     List<BoardDto> selectBoardList() throws Exception;
-    void insertBoard(BoardDto board) throws Exception;  // 게시글 작성 요청 처리
+    int insertBoard(BoardDto board) throws Exception;  // 게시글 작성 요청 처리
     BoardDto selectBoardDetail(int board_id) throws Exception;
     void updateBoard(BoardDto board) throws Exception;
     void deleteBoard(int board_id) throws Exception;
