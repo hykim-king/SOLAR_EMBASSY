9eefceb07006b1dfe2f0ba447f8b070e45677eeb
kimyusuk <airheart0820@gmail.com>
2023-02-13 15:29:46 +0900

    main pull 오후 3:29 23-02-13

--- doc/README.txt
+++ doc/README.txt
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-push 테스트adasdasgit
-=======
-push 테스트 ... eeeee
->>>>>>> 3d8afd583d50147f12e96d62c363fef61f14fd29
+push 테스트
--- solar_embassy_community/src/main/java/com/community/solar_embassy/service/UsersService.java
+++ solar_embassy_community/src/main/java/com/community/solar_embassy/service/UsersService.java
@@ -5,4 +5,6 @@ import com.community.solar_embassy.dto.Users;
 public interface UsersService {
 
     Users login(String userId, String pw);
+
+    int signup_normal(Users users);
 }
