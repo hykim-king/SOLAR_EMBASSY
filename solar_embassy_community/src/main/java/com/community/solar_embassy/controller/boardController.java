3d564cbd9033f626cf82692d18ab392f283e60f1
문현희 <119906623+MOONHYUNHEE@users.noreply.github.com>
2023-02-14 16:55:33 +0900

    상세 부분 수정
    상세 부분 수정

--- solar_embassy_community/src/main/java/com/community/solar_embassy/controller/boardController.java
+++ solar_embassy_community/src/main/java/com/community/solar_embassy/controller/boardController.java
@@ -2,19 +2,17 @@ package com.community.solar_embassy.controller;
 
 import com.community.solar_embassy.dto.BoardDto;
 import com.community.solar_embassy.service.BoardService;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Locale;
 
 @Controller
 @RequestMapping("/board")
-public class boardController {
+public class BoardController {
 
     @Autowired
     BoardService boardService;
@@ -49,10 +47,13 @@ public class boardController {
     }
 
     @RequestMapping("/boardDetail")
-    public String boardDetail(@RequestParam int board_no) throws Exception {
-        boardService.selectBoardDetail(board_no);
+    public String boardDetail(@RequestParam int boardNo, Model model) throws Exception {
+        BoardDto board = boardService.selectBoardDetail(boardNo);
+        board.setBoardNo(boardNo);
+        model.addAttribute("board",board);
         return "/boardDetail";
     }
+//     return "redirect:  (설정한 링크로 가는 것 ) / html로 가면 redirect 제외해야 함
 
     //    @RequestMapping("/openBoardDetail")
 //    public ModelAndView openBoardDetail(@RequestParam int board_no) throws Exception{
@@ -68,10 +69,18 @@ public class boardController {
     }
 
 
-    @RequestMapping("/deleteBoard")  //삭제요청
-    public String deleteBoard(BoardDto board) throws Exception {
-        boardService.deleteBoard(board.getBoardNo());      //게시글 삭제
-        return "redirect:/board/boardList";  //삭제완료 후 게시판 목록으로
+    @RequestMapping (value = "deleteBoard", method = RequestMethod.POST)
+    public String deleteBoard(@RequestParam int boardNo) throws Exception {
+        boardService.deleteBoard(boardNo);
+        return "redirect:/board/boardList.do";
     }
+
+
+
+//    @RequestMapping("/deleteBoard")  //삭제요청
+//    public String deleteBoard(BoardDto board) throws Exception {
+//        boardService.deleteBoard(board.getBoardNo());      //게시글 삭제
+//        return "redirect:/board/boardList";  //삭제완료 후 게시판 목록으로
+//    }
 
 }
