cd4e30c266e2e235626e1a30105e28ec5c333d93
정효빈 <119907043+hyobean@users.noreply.github.com>
2023-02-25 12:18:17 +0900

    자동차백과 작업 추가

--- solar_embassy_community/src/main/java/com/community/solar_embassy/controller/AutoController.java
+++ solar_embassy_community/src/main/java/com/community/solar_embassy/controller/AutoController.java
@@ -1,33 +1,60 @@
 package com.community.solar_embassy.controller;
 
 import com.community.solar_embassy.dto.Auto;
+import com.community.solar_embassy.dto.Company;
 import com.community.solar_embassy.service.AutoService;
+import com.community.solar_embassy.service.CompanyService;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mapper
 @Controller
 @RequestMapping("/galaxy")
 public class AutoController {
-    private AutoService autoService;
+    @Autowired
+    AutoService autoService;
+    CompanyService companyService;
+    //AutoImgService autoImgService;
 
-    public AutoController(AutoService autoService) {
+    private Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
+    public AutoController(AutoService autoService, CompanyService companyService
+                          ) {
         this.autoService = autoService;
+        this.companyService = companyService;
+        //this.autoImgService = autoImgService;
     }
 
     @GetMapping("/autopedia.do")
-    public void autopedia(){}
+    public void autopedia() {}
 
     @GetMapping("/autoDetail.do")
     public ModelAndView autoDetail(
             @RequestParam(name = "autoName") String autoName,
+            //@RequestParam(name = "autoImgNo") int autoImgNo,
             ModelAndView model) throws Exception {
         Auto auto = autoService.detail(autoName);
+        //AutoImg autoImg = autoImgService.detail(autoImgNo);
         model.addObject("auto", auto);
+        //model.addObject("autoImg", autoImg);
         model.setViewName("/galaxy/autoDetail");
         return model;
-    }}
+    }
+
+    @GetMapping("/comDetail.do")
+    public ModelAndView comDetail(
+            @RequestParam(name = "comName") String comName,
+            ModelAndView model) throws Exception {
+        Company company = companyService.detail(comName);
+        model.addObject("company", company);
+        model.setViewName("/galaxy/comDetail");
+        return model;
+    }
+}
